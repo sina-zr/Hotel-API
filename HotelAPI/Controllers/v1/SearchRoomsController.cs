94b86b8f23a0a65e7b52461301fb4f3ada0f9cbe
Sina <sinazare1381@gmail.com>
2023-10-19 11:48:42 +0330

    Added GetAvailableRoomTypes Endpoint and its UnitTesting via Moq.

---
+++ HotelAPI/Controllers/v1/SearchRoomsController.cs
@@ -0,0 +1,50 @@
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelAPI.Controllers.v1
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class SearchRoomsController : ControllerBase
+    {
+        private readonly HotelContext _db;
+
+        public SearchRoomsController(HotelContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// Get Available Room Types (not Rooms) and their info
+        /// like Title, Description and Price
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns> List of availableRoomTypes wrraped in IACtionResult </returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
+        {
+            try
+            {
+                var availableRoomTypes = await _db.RoomTypes
+                    .Where(room =>
+                        !_db.Bookings.Any(booking =>
+                            booking.RoomId == room.Id &&
+                            startDate < booking.EndDate &&
+                            endDate > booking.StartDate))
+                    .ToListAsync();
+
+                return Ok(availableRoomTypes);
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions
+                return StatusCode(500, $"An error occurred while processing your request.");
+            }
+        }
+    }
+}
---
+++ UnitTests/v1/SearchRoomsControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using EFDataAccessLibrary.DataAccess;
+using HotelAPI.Controllers.v1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTests.v1
+{
+    public class SearchRoomsControllerTests
+    {
+        [Fact]
+        public async Task GetAvailableRoomTypes_Returns_OkResult()
+        {
+            // Arrange
+
+            // We set up an in-memory database for testing using Entity Framework Core
+            var options = new DbContextOptionsBuilder<HotelContext>()
+                .UseInMemoryDatabase(databaseName: "InMemoryDatabase")
+                .Options;
+
+            using var context = new HotelContext(options);
+
+            // We create an instance of the controller, passing the in-memory database context.
+            var controller  = new SearchRoomsController(context);
+
+
+            // Act
+            var result = await controller.GetAvailableRoomTypes(DateTime.Now, DateTime.Now.AddDays(1));
+
+            // Assert
+
+            // We call the action method and assert that it returns an OkObjectResult.
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
