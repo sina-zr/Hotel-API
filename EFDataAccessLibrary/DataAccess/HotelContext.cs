1927b9ac794d4c4971c0cb47b3851ae88b0e3c7b
Sina <sinazare1381@gmail.com>
2023-10-19 17:21:16 +0330

    Added SaveChangesAsync() method to IHotelContext inteface.

--- EFDataAccessLibrary/DataAccess/HotelContext.cs
+++ EFDataAccessLibrary/DataAccess/HotelContext.cs
@@ -6,15 +6,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EFDataAccessLibrary.DataAccess
-{
-    public class HotelContext : DbContext
-    {
-        public HotelContext(DbContextOptions options) : base(options) { }
+namespace EFDataAccessLibrary.DataAccess;
 
-        public DbSet<RoomType> RoomTypes { get; set; }
-        public DbSet<Room> Rooms {  get; set; }
-        public DbSet<Guest> Guests { get; set; }
-        public DbSet<Booking> Bookings { get; set; }
+public class HotelContext : DbContext, IHotelContext
+{
+    public HotelContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<RoomType> RoomTypes { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Guest> Guests { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await base.SaveChangesAsync(cancellationToken);
     }
 }
---
+++ EFDataAccessLibrary/DataAccess/IHotelContext.cs
@@ -0,0 +1,15 @@
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace EFDataAccessLibrary.DataAccess;
+
+public interface IHotelContext
+{
+    DbSet<Booking> Bookings { get; set; }
+    DbSet<Guest> Guests { get; set; }
+    DbSet<Room> Rooms { get; set; }
+    DbSet<RoomType> RoomTypes { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
