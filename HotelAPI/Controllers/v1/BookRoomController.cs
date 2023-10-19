816cb665c6630d6d371e03a5015d8be626c0a47b
Sina <sinazare1381@gmail.com>
2023-10-19 18:38:40 +0330

    Created async metods for BookARoom Endpoint logics.

--- HotelAPI/Controllers/v1/BookRoomController.cs
+++ HotelAPI/Controllers/v1/BookRoomController.cs
@@ -2,6 +2,7 @@
 using EFDataAccessLibrary.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelAPI.Controllers.v1;
 
@@ -36,41 +37,72 @@ public class BookRoomController : ControllerBase
             firstName = firstName.ToLower();
             lastName = lastName.ToLower();
 
-            var availableRooms = _db.Rooms
-                .Where(room => room.RoomTypeId == roomTypeId &&
-                    !_db.Bookings.Any(booking =>
-                        booking.RoomId == room.Id &&
-                        startDate < booking.EndDate &&
-                        endDate > booking.StartDate))
-                .ToList();
+            var availableRooms = await GetAvailableRoomsAsync(roomTypeId, startDate, endDate);
 
-            // Checking if user exists
-            var guest = _db.Guests.FirstOrDefault(g => g.FirstName == firstName && g.LastName == lastName);
+            var guest = await GetOrCreateGuestAsync(firstName, lastName, email);
 
-            if (guest == null)
+            if (availableRooms.Any())
             {
-                guest = new Guest { FirstName = firstName, LastName = lastName, EmailAddress = email };
-                await _db.Guests.AddAsync(guest);
+                var roomId = availableRooms.First().Id;
+                var totalCost = CalculateTotalCost(roomTypeId, startDate, endDate);
+
+                var booking = CreateBooking(roomId, guest.Id, startDate, endDate, totalCost);
+
+                await _db.Bookings.AddAsync(booking);
                 await _db.SaveChangesAsync();
 
-                // Get User Id
-                guest = _db.Guests.Where(g => g.FirstName == firstName && g.LastName == lastName).FirstOrDefault();
+                return Ok();
             }
-
-            var roomId = availableRooms.First().Id;
-            var roomType = _db.RoomTypes.Where(roomType => roomType.Id == roomTypeId).First();
-            var totalCost = roomType.Price * endDate.Subtract(startDate).Days;
-
-            var booking = new Booking { RoomId = roomId, GuestId = guest!.Id, StartDate = startDate, EndDate = endDate, CheckedIn = false, TotalCost = totalCost };
-
-            await _db.Bookings.AddAsync(booking);
-            await _db.SaveChangesAsync();
-
-            return Ok();
+            else
+            {
+                return StatusCode(400, "No available rooms for the specified dates.");
+            }
         }
         catch (Exception)
         {
-            return StatusCode(500, $"An error occurred while processing your request.");
+            return StatusCode(500, "An error occurred while processing your request.");
         }
+    }
+
+    private async Task<List<Room>> GetAvailableRoomsAsync(int roomTypeId, DateTime startDate, DateTime endDate)
+    {
+        var rooms = await _db.Rooms
+                .Where(room => room.RoomTypeId == roomTypeId)
+                .ToListAsync(); // Assuming ToListAsync is available for async retrieval
+
+        var existingBookings = await _db.Bookings
+            .Where(booking => startDate < booking.EndDate && endDate > booking.StartDate)
+        .ToListAsync(); // Assuming ToListAsync is available for async retrieval
+
+        return rooms.Where(room => !existingBookings.Any(booking => booking.RoomId == room.Id)).ToList();
+    }
+
+    private async Task<Guest> GetOrCreateGuestAsync(string firstName, string lastName, string email)
+    {
+        var guest = await _db.Guests.FirstOrDefaultAsync(g => g.FirstName == firstName && g.LastName == lastName);
+
+        if (guest == null)
+        {
+            guest = new Guest { FirstName = firstName, LastName = lastName, EmailAddress = email };
+            _db.Guests.Add(guest);
+            await _db.SaveChangesAsync();
+        }
+
+        return guest;
+    }
+
+    private decimal CalculateTotalCost(int roomTypeId, DateTime startDate, DateTime endDate)
+    {
+        var roomType = _db.RoomTypes.FirstOrDefault(rt => rt.Id == roomTypeId);
+        if (roomType != null)
+        {
+            return roomType.Price * endDate.Subtract(startDate).Days;
+        }
+        return 0; // Handle the case when room type is not found.
+    }
+
+    private Booking CreateBooking(int roomId, int guestId, DateTime startDate, DateTime endDate, decimal totalCost)
+    {
+        return new Booking { RoomId = roomId, GuestId = guestId, StartDate = startDate, EndDate = endDate, CheckedIn = false, TotalCost = totalCost };
     }
 }
